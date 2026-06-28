649f7e8e930190fded8bdd9d2460b9d81baeeb01
Sneha <bsneha427@gmail.com>
2026-06-28 14:10:57 +0530

    Add employee create and fetch APIs

---
+++ backend/employee-management-backend/src/main/java/com/sneha/employee_management_backend/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.sneha.employee_management_backend.controller;
+
+
+import com.sneha.employee_management_backend.entity.Employee;
+import com.sneha.employee_management_backend.service.EmployeeService;
+
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    @PostConstruct
+    public void init() {
+        System.out.println("EmployeeController Loaded");
+    }
+
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // Add Employee
+    @PostMapping
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    // Get All Employees
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
---
+++ backend/employee-management-backend/src/main/java/com/sneha/employee_management_backend/service/EmployeeService.java
@@ -0,0 +1,26 @@
+package com.sneha.employee_management_backend.service;
+
+
+import com.sneha.employee_management_backend.entity.Employee;
+import com.sneha.employee_management_backend.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
